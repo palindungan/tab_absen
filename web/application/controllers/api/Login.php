ed6fad39b228088bfc155a2150950d7ec3069290
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-22 00:40:39 +0700

    Update Login.php

--- web/application/controllers/api/Login.php
+++ web/application/controllers/api/Login.php
@@ -11,25 +11,29 @@ class Login extends REST_Controller
     function __construct($config = 'rest')
     {
         parent::__construct($config);
-        $this->load->database();
         $this->load->model("api/M_login");
     }
 
     //Mengirim atau menambah data kontak baru
     function index_post()
     {
-
+        // ambil data
         $username = $this->post('username');
         $password = $this->post('password');
 
+        // data array untuk where db
+        $where = array(
+            'username' => $username
+        );
+
         // mengambil jumlah baris
-        $cek = $this->M_login->ambil_data($username)->num_rows();
+        $cek = $this->M_login->get_data('admin', $where)->num_rows();
 
         // cek apakah ada data dari username
         if ($cek > 0) {
 
             // mengambil data dari database berdasarkan username
-            $query = $this->M_login->ambil_data($username);
+            $query = $this->M_login->get_data('admin', $where);
 
             // variable array
             $result = array();
@@ -41,33 +45,31 @@ class Login extends REST_Controller
                 // dicek apakah data inputan sama dengan data di database
                 if (password_verify($password, $row["password"])) {
 
-                    // kumpulan data
+                    // ambil detail data db
                     $data = array(
-                        'id_customer' => $row["id_customer"],
-                        'nm_customer' => $row["nm_customer"],
-                        'almt_customer' => $row["almt_customer"],
-                        'jenkel_customer' => $row["jenkel_customer"],
-                        'no_hp' => $row["no_hp"],
-                        'email' => $row["email"]
+                        'id_admin' => $row["id_admin"],
+                        'nama' => $row["nama"],
+                        'username' => $row["username"],
+                        'foto' => $row["foto"]
                     );
 
                     array_push($result['login'], $data);
 
                     // membuat array untuk di transfer
                     $result["success"] = "1";
-                    $result["message"] = "success";
+                    $result["message"] = "success berhasil masuk";
                     $this->response($result, 200);
                 } else {
                     // membuat array untuk di transfer ke API
                     $result["success"] = "0";
-                    $result["message"] = "error password";
+                    $result["message"] = "error password anda salah";
                     $this->response($result, 502);
                 }
             }
         } else {
             // membuat array untuk di transfer ke API
             $result["success"] = "0";
-            $result["message"] = "error username";
+            $result["message"] = "error username tidak ditemukan";
             $this->response($result, 404);
         }
     }
