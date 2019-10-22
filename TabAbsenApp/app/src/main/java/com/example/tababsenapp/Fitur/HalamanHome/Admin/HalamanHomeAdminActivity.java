2b79c7844e6c9b0a465dd5e38f71b8abf0ceb756
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-22 23:11:14 +0700

    persiapan awal halaman home
    persiapan awal halaman home

---
+++ TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanHome/Admin/HalamanHomeAdminActivity.java
@@ -0,0 +1,16 @@
+package com.example.tababsenapp.Fitur.HalamanHome.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tababsenapp.R;
+
+public class HalamanHomeAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_home_admin);
+    }
+}
