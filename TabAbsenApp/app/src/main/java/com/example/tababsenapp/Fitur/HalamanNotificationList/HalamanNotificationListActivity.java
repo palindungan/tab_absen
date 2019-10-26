f152c4477bab5620293a16450ad946fedbe4b4a3
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-26 21:46:06 +0700

    back app bar notification
    back app bar notification

--- TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanNotificationList/HalamanNotificationListActivity.java
+++ TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanNotificationList/HalamanNotificationListActivity.java
@@ -1,16 +1,42 @@
 package com.example.tababsenapp.Fitur.HalamanNotificationList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.tababsenapp.R;
 
 public class HalamanNotificationListActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_notification_list);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        initActionBar();
+    }
+
+    private void initActionBar() {
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
     }
 }
