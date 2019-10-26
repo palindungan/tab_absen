ed7a5fc14bf0f84b33de1db916e09bcb5d59be94
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-27 02:45:59 +0700

    persiapan model data pengajar
    persiapan model data pengajar

--- TabAbsenApp/app/src/main/java/com/example/tababsenapp/Model/pengajar/IPengajar.java
+++ TabAbsenApp/app/src/main/java/com/example/tababsenapp/Model/pengajar/IPengajar.java
@@ -1,4 +1,10 @@
 package com.example.tababsenapp.Model.pengajar;
 
 public interface IPengajar {
+    String getIdPengajar();
+    String getNama();
+    String getUsername();
+    String getAlamat();
+    String getNoHP();
+    String getFoto();
 }
--- TabAbsenApp/app/src/main/java/com/example/tababsenapp/Model/pengajar/Pengajar.java
+++ TabAbsenApp/app/src/main/java/com/example/tababsenapp/Model/pengajar/Pengajar.java
@@ -1,4 +1,45 @@
 package com.example.tababsenapp.Model.pengajar;
 
-public class Pengajar {
+public class Pengajar implements IPengajar {
+
+    String id_pengajar, nama, username, alamat, no_hp, foto;
+
+    public Pengajar(String id_pengajar, String nama, String username, String alamat, String no_hp, String foto) {
+        this.id_pengajar = id_pengajar;
+        this.nama = nama;
+        this.username = username;
+        this.alamat = alamat;
+        this.no_hp = no_hp;
+        this.foto = foto;
+    }
+
+    @Override
+    public String getIdPengajar() {
+        return id_pengajar;
+    }
+
+    @Override
+    public String getNama() {
+        return nama;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getAlamat() {
+        return alamat;
+    }
+
+    @Override
+    public String getNoHP() {
+        return no_hp;
+    }
+
+    @Override
+    public String getFoto() {
+        return foto;
+    }
 }
