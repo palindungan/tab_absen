761d0ffd3e2b7e8aec22ebc19c24b266c1ef42f2
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-27 14:47:42 +0700

    persiapan awal edit dan tambah pengajar
    persiapan awal edit dan tambah pengajar

---
+++ TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanFormPengajar/Edit/HalamanFormEditPengajarActivity.java
@@ -0,0 +1,16 @@
+package com.example.tababsenapp.Fitur.HalamanFormPengajar.Edit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tababsenapp.R;
+
+public class HalamanFormEditPengajarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_form_edit_pengajar);
+    }
+}
---
+++ TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanFormPengajar/Tambah/HalamanFormTambahPengajarActivity.java
@@ -0,0 +1,16 @@
+package com.example.tababsenapp.Fitur.HalamanFormPengajar.Tambah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tababsenapp.R;
+
+public class HalamanFormTambahPengajarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_form_tambah_pengajar);
+    }
+}
