cad787981beb8273784a56d41636e73036cf3e95
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-11-10 20:35:24 +0700

    uytyt

--- TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanListWaliMurid/HalamanListWaliMuridActivity.java
+++ TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanListWaliMurid/HalamanListWaliMuridActivity.java
@@ -1,14 +1,19 @@
 package com.example.tababsenapp.Fitur.HalamanListWaliMurid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.widget.Toolbar;
 
+import com.example.tababsenapp.Adapters.AdapterDaftarWaliMurid;
 import com.example.tababsenapp.Fitur.HalamanListWaliMurid.presenter.IListWaliMuridPresenter;
+import com.example.tababsenapp.Fitur.HalamanListWaliMurid.presenter.ListWaliMuridPresenter;
 import com.example.tababsenapp.Fitur.HalamanListWaliMurid.view.IListWaliMuridView;
 import com.example.tababsenapp.Model.waliMurid.WaliMurid;
 import com.example.tababsenapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -16,12 +21,28 @@ public class HalamanListWaliMuridActivity extends AppCompatActivity implements I
 
     IListWaliMuridPresenter listWaliMuridPresenter;
 
+    private AdapterDaftarWaliMurid adapterDaftarWaliMurid;
+    private RecyclerView recyclerView;
+
     Toolbar toolbar;
+
+    String EXTRA_ID_WALI_MURID = "EXTRA_ID_WALI_MURID";
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_list_wali_murid);
+
+        listWaliMuridPresenter = new ListWaliMuridPresenter(this,this);
+        listWaliMuridPresenter.onLoadSemuaListWaliMurid();
+
+        recyclerView = findViewById(R.id.recycle_list_wali_murid);
+
+        toolbar = findViewById(R.id.toolbar);
+        initActionBar();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
     }
 
     @Override
--- TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanListWaliMurid/presenter/ListWaliMuridPresenter.java
+++ TabAbsenApp/app/src/main/java/com/example/tababsenapp/Fitur/HalamanListWaliMurid/presenter/ListWaliMuridPresenter.java
@@ -1,6 +1,32 @@
 package com.example.tababsenapp.Fitur.HalamanListWaliMurid.presenter;
 
+import android.content.Context;
+
+import com.example.tababsenapp.Controllers.SessionManager;
+import com.example.tababsenapp.Fitur.HalamanListWaliMurid.view.IListWaliMuridView;
+import com.example.tababsenapp.Model.waliMurid.WaliMurid;
+
+import java.util.ArrayList;
+
 public class ListWaliMuridPresenter implements IListWaliMuridPresenter {
+
+    IListWaliMuridView listWaliMuridView;
+    Context context;
+
+    SessionManager sessionManager;
+
+    ArrayList<WaliMurid> dataModelArrayList;
+
+    String base_url;
+
+    public ListWaliMuridPresenter(IListWaliMuridView listWaliMuridView, Context context) {
+        this.listWaliMuridView = listWaliMuridView;
+        this.context = context;
+
+        sessionManager = new SessionManager(context);
+        base_url = sessionManager.getBaseUrl();
+    }
+
     @Override
     public void onLoadSemuaListWaliMurid() {
 
