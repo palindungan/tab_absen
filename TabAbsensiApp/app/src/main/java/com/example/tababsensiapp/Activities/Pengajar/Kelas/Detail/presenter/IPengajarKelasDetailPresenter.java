7a2e02e011d7c91fc701a7f50424a995626097a9
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-12-17 23:10:35 +0700

    asd
    asd

--- TabAbsensiApp/app/src/main/java/com/example/tababsensiapp/Activities/Pengajar/Kelas/Detail/presenter/IPengajarKelasDetailPresenter.java
+++
@@ -1,7 +0,0 @@
-package com.example.tababsensiapp.Activities.Pengajar.Kelas.Detail.presenter;
-
-public interface IPengajarKelasDetailPresenter {
-    void onLoadSemuaData(String id);
-
-    void onLoadSemuaDataKelas(String id);
-}
