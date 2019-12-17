344a11bb07f799592c6d8a4b109df372222c7fee
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-12-17 21:55:39 +0700

    persiapan
    persiapan

--- TabAbsensiApp/app/src/main/java/com/example/tababsensiapp/Activities/Pengajar/Kelas/TampilSemua/PengajarKelasTampilSemuaActivity.java
+++ TabAbsensiApp/app/src/main/java/com/example/tababsensiapp/Activities/Pengajar/Kelas/TampilSemua/PengajarKelasTampilSemuaActivity.java
@@ -1,22 +1,82 @@
 package com.example.tababsensiapp.Activities.Pengajar.Kelas.TampilSemua;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.tababsensiapp.Activities.Pengajar.Kelas.TampilSemua.presenter.IPengajarKelasTampilSemuaPresenter;
+import com.example.tababsensiapp.Activities.Pengajar.Kelas.TampilSemua.presenter.PengajarKelasTampilSemuaPresenter;
 import com.example.tababsensiapp.Activities.Pengajar.Kelas.TampilSemua.view.IPengajarKelasTampilSemuaView;
+import com.example.tababsensiapp.Adapters.AdapterDaftarKelas;
+import com.example.tababsensiapp.Controllers.SessionManager;
 import com.example.tababsensiapp.Models.Kelas;
 import com.example.tababsensiapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import es.dmoral.toasty.Toasty;
 
 public class PengajarKelasTampilSemuaActivity extends AppCompatActivity implements View.OnClickListener, IPengajarKelasTampilSemuaView {
+
+    IPengajarKelasTampilSemuaPresenter pengajarKelasTampilSemuaPresenter;
+
+    private AdapterDaftarKelas adapterDaftarKelas;
+    private RecyclerView recyclerView;
+
+    Toolbar toolbar;
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    FloatingActionButton fab;
+
+    SessionManager sessionManager;
+    String id_pengajar = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengajar_kelas_tampil_semua);
+
+        sessionManager = new SessionManager(this);
+
+        pengajarKelasTampilSemuaPresenter = new PengajarKelasTampilSemuaPresenter(this, this);
+        HashMap<String, String> user = sessionManager.getDataUser();
+        id_pengajar = user.get(sessionManager.ID_USER);
+        pengajarKelasTampilSemuaPresenter.inisiasiAwal(id_pengajar);
+
+        recyclerView = findViewById(R.id.recycle_view);
+
+        toolbar = findViewById(R.id.toolbar);
+        initActionBar();
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to make your refresh action
+                pengajarKelasTampilSemuaPresenter.inisiasiAwal(id_pengajar);
+
+                // CallYourRefreshingMethod();
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (swipeRefreshLayout.isRefreshing()) {
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
+                    }
+                }, 1000);
+            }
+        });
     }
 
     @Override
@@ -26,21 +86,57 @@ public class PengajarKelasTampilSemuaActivity extends AppCompatActivity implemen
 
     @Override
     public void initActionBar() {
-
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
     public void onSetupListView(ArrayList<Kelas> dataModelArrayList) {
+        adapterDaftarKelas = new AdapterDaftarKelas(this, dataModelArrayList);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 1, GridLayoutManager.VERTICAL, false);
+        recyclerView.setAdapter(adapterDaftarKelas);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setNestedScrollingEnabled(true);
+        adapterDaftarKelas.notifyDataSetChanged();
 
+        adapterDaftarKelas.setOnItemClickListener(new AdapterDaftarKelas.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+//                Intent intent = new Intent(getApplicationContext(), AdminKelasDetailKelasActivity.class);
+//                intent.putExtra(AdminKelasDetailKelasActivity.EXTRA_ID_KELAS_P, dataModelArrayList.get(position).getId_kelas_p());
+//                intent.putExtra(AdminKelasDetailKelasActivity.EXTRA_ID_MATA_PELAJARAN, dataModelArrayList.get(position).getId_mata_pelajaran());
+//                intent.putExtra(AdminKelasDetailKelasActivity.EXTRA_ID_PENGAJAR, dataModelArrayList.get(position).getId_pengajar());
+//                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public void onSuccessMessage(String message) {
-
+        Toasty.success(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onErrorMessage(String message) {
+        Toasty.error(this, message, Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        pengajarKelasTampilSemuaPresenter.inisiasiAwal(id_pengajar);
     }
 }
--- TabAbsensiApp/app/src/main/java/com/example/tababsensiapp/Activities/Pengajar/Kelas/TampilSemua/presenter/PengajarKelasTampilSemuaPresenter.java
+++ TabAbsensiApp/app/src/main/java/com/example/tababsensiapp/Activities/Pengajar/Kelas/TampilSemua/presenter/PengajarKelasTampilSemuaPresenter.java
@@ -1,8 +1,120 @@
 package com.example.tababsensiapp.Activities.Pengajar.Kelas.TampilSemua.presenter;
 
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.tababsensiapp.Activities.Pengajar.Kelas.TampilSemua.view.IPengajarKelasTampilSemuaView;
+import com.example.tababsensiapp.Controllers.BaseUrl;
+import com.example.tababsensiapp.Models.Kelas;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PengajarKelasTampilSemuaPresenter implements IPengajarKelasTampilSemuaPresenter {
+
+    Context context;
+    IPengajarKelasTampilSemuaView pengajarKelasTampilSemuaView;
+
+    BaseUrl baseUrl;
+
+    ArrayList<Kelas> dataModelArrayList;
+
+    public PengajarKelasTampilSemuaPresenter(Context context, IPengajarKelasTampilSemuaView pengajarKelasTampilSemuaView) {
+        this.context = context;
+        this.pengajarKelasTampilSemuaView = pengajarKelasTampilSemuaView;
+
+        baseUrl = new BaseUrl();
+    }
+
     @Override
     public void inisiasiAwal(String id_pengajar) {
+        String base_url = baseUrl.getUrlData();
+        String URL_DATA = base_url + "admin/kelas_pertemuan/ambil_data_kelas_pertemuan"; // url http request
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_DATA,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+
+                            if (obj.optString("success").equals("1")) {
+
+                                dataModelArrayList = new ArrayList<>();
+                                JSONArray dataArray = obj.getJSONArray("list_kelas");
+                                for (int i = 0; i < dataArray.length(); i++) {
+
+                                    Kelas playerModel = new Kelas();
+                                    JSONObject dataobj = dataArray.getJSONObject(i);
+
+                                    String id_kelas_p = dataobj.getString("id_kelas_p");
+                                    String hari = dataobj.getString("hari");
+                                    String jam_mulai = dataobj.getString("jam_mulai");
+                                    String jam_berakhir = dataobj.getString("jam_berakhir");
+                                    String harga_fee = dataobj.getString("harga_fee");
+                                    String nama_pelajaran = dataobj.getString("nama_pelajaran");
+                                    String nama_sharing = dataobj.getString("nama_sharing");
+
+                                    String id_mata_pelajaran = dataobj.getString("id_mata_pelajaran");
+                                    String id_pengajar = dataobj.getString("id_pengajar");
+                                    String id_sharing = dataobj.getString("id_sharing");
+                                    String nama_pengajar = dataobj.getString("nama_pengajar");
+
+                                    playerModel.setId_kelas_p(id_kelas_p);
+                                    playerModel.setHari(hari);
+                                    playerModel.setJam_mulai(jam_mulai);
+                                    playerModel.setJam_berakhir(jam_berakhir);
+                                    playerModel.setHarga_fee(harga_fee);
+                                    playerModel.setNama_pelajaran(nama_pelajaran);
+                                    playerModel.setNama_sharing(nama_sharing);
+
+                                    playerModel.setId_mata_pelajaran(id_mata_pelajaran);
+                                    playerModel.setId_pengajar(id_pengajar);
+                                    playerModel.setId_sharing(id_sharing);
+                                    playerModel.setNama_pengajar(nama_pengajar);
+
+                                    dataModelArrayList.add(playerModel);
+                                }
+
+                                pengajarKelasTampilSemuaView.onSetupListView(dataModelArrayList);
+                            } else {
+                                dataModelArrayList = new ArrayList<>();
+                                pengajarKelasTampilSemuaView.onSetupListView(dataModelArrayList);
+                                pengajarKelasTampilSemuaView.onErrorMessage("Database Kosong Silahkan Menambah Data Baru !");
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            pengajarKelasTampilSemuaView.onErrorMessage("Kesalahan Menerima Data : " + e.toString());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pengajarKelasTampilSemuaView.onErrorMessage("Volley Error : " + error.toString());
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("id_pengajar", id_pengajar);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
     }
 }
