0fc3b74f45ad795213ffc4da7591c84abb3a28b2
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-04-30 14:18:38 +0700

    gradle kecuali gmaps
    gradle kecuali gmaps

--- ANDROID/BigStars/app/src/main/java/com/its/bigstars/Activities/_Main/MainActivity.java
+++ ANDROID/BigStars/app/src/main/java/com/its/bigstars/Activities/_Main/MainActivity.java
@@ -3,14 +3,21 @@ package com.its.bigstars.Activities._Main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.its.bigstars.Activities._Main.view.IMainView;
 import com.its.bigstars.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, IMainView {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ ANDROID/BigStars/app/src/main/java/com/its/bigstars/Activities/_Main/view/IMainView.java
@@ -0,0 +1,4 @@
+package com.its.bigstars.Activities._Main.view;
+
+public interface IMainView {
+}
