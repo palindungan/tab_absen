29bc3df35fddfce5c71bd8d28f74c885d14188fc
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-05-04 14:45:22 +0700

    sebelum api
    sebelum api

--- ANDROID/BigStars/app/src/main/java/com/its/bigstars/Activities/Data/Murid/List/presenter/DataMuridListPresenter.java
+++ ANDROID/BigStars/app/src/main/java/com/its/bigstars/Activities/Data/Murid/List/presenter/DataMuridListPresenter.java
@@ -2,21 +2,103 @@ package com.its.bigstars.Activities.Data.Murid.List.presenter;
 
 import android.content.Context;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.its.bigstars.Activities.Data.Murid.List.view.IDataMuridListView;
+import com.its.bigstars.Controllers.BaseUrl;
+import com.its.bigstars.Controllers.GlobalMessage;
+import com.its.bigstars.Controllers.ToastMessage;
+import com.its.bigstars.Models.Murid;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class DataMuridListPresenter implements IDataMuridListPresenter {
 
     Context context;
     IDataMuridListView dataMuridListView;
 
+    BaseUrl baseUrl;
+    GlobalMessage globalMessage;
+    ToastMessage toastMessage;
+
+    ArrayList<Murid> dataModelArrayList;
+
     public DataMuridListPresenter(Context context, IDataMuridListView dataMuridListView) {
         this.context = context;
         this.dataMuridListView = dataMuridListView;
+
+        baseUrl = new BaseUrl();
+        globalMessage = new GlobalMessage();
+        toastMessage = new ToastMessage(context);
     }
 
     @Override
     public void onLoadDataList() {
+        String base_url = baseUrl.getUrlData();
+        String URL_DATA = base_url + "data/murid/list_murid"; // url http request
 
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_DATA, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+
+                    JSONObject obj = new JSONObject(response);
+
+                    String success = obj.getString("success");
+                    String message = obj.getString("message");
+
+                    if (success.equals("1")) {
+
+                        dataModelArrayList = new ArrayList<>();
+                        JSONArray dataArray = obj.getJSONArray("data_result");
+                        for (int i = 0; i < dataArray.length(); i++) {
+
+                            Murid playerModel = new Murid();
+                            JSONObject dataobj = dataArray.getJSONObject(i);
+
+                            String id_murid = dataobj.getString("id_murid");
+                            String nama = dataobj.getString("nama");
+                            String nama_wali_murid = dataobj.getString("nama_wali_murid");
+                            String alamat = dataobj.getString("alamat");
+                            String foto = dataobj.getString("foto");
+
+                            playerModel.setId_murid(id_murid);
+                            playerModel.setNama(nama);
+                            playerModel.setNama_wali_murid(nama_wali_murid);
+                            playerModel.setAlamat(alamat);
+                            playerModel.setFoto(foto);
+
+                            dataModelArrayList.add(playerModel);
+                        }
+                        dataMuridListView.onSetupListView(dataModelArrayList);
+
+                    } else {
+                        dataModelArrayList = new ArrayList<>();
+                        dataMuridListView.onSetupListView(dataModelArrayList);
+                        toastMessage.onErrorMessage(message);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    toastMessage.onErrorMessage(globalMessage.getMessageResponseError() + e.toString());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                toastMessage.onErrorMessage(globalMessage.getMessageConnectionError());
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
     }
 
     @Override
---
+++ web/application/controllers/api/data/Murid.php
@@ -0,0 +1,240 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+use Restserver\Libraries\REST_Controller;
+
+class Murid extends REST_Controller
+{
+
+    function __construct($config = 'rest')
+    {
+        parent::__construct($config);
+        $this->load->model("api/M_universal");
+        $this->load->model("api/M_kode");
+    }
+
+    function list_murid_get()
+    {
+        // mengambil data dari database
+        $query = $this->M_universal->tampil_data('list_murid');
+
+        // variable array
+        $result = array();
+        $result['murid'] = array();
+
+        if ($query->num_rows() > 0) {
+
+            // mengeluarkan data dari database
+            foreach ($query->result_array() as $row) {
+
+                // ambil detail data db
+                $data = array(
+                    'id_murid' => $row["id_murid"],
+                    'nama' => $row["nama"],
+                    'nama_wali_murid' => $row["nama_wali_murid"],
+                    'alamat' => $row["alamat"],
+                    'foto' => $row["foto"]
+                );
+
+                array_push($result['murid'], $data);
+
+                // membuat array untuk di transfer
+                $result["success"] = "1";
+                $result["message"] = "success berhasil mengambil data";
+                $this->response($result, 200);
+            }
+        } else {
+            // membuat array untuk di transfer ke API
+            $result["success"] = "0";
+            $result["message"] = "error data tidak ada";
+            $this->response($result, 200);
+        }
+    }
+
+    function tambah_murid_post()
+    {
+        // ambil data
+        $id_murid = $this->M_kode->get_id_murid();
+        $id_wali_murid = $this->post('id_wali_murid');
+        $nama = $this->post('nama');
+        $foto = $this->post('foto');
+
+        $nama_foto = "DEFFMR";
+
+        if (!empty($foto)) {
+            $nama_foto = 'F' . $id_murid;
+        }
+
+        $data = array(
+            'id_murid'   => $id_murid,
+            'id_wali_murid'   => $id_wali_murid,
+            'nama'   => $nama,
+            'foto'          => $nama_foto
+        );
+
+        $insert =  $this->M_universal->input_data('murid', $data);
+        if ($insert) {
+
+            if (!empty($foto)) {
+                $path = "./upload/image/murid/$nama_foto.jpg";
+                file_put_contents($path, base64_decode($foto));
+            }
+
+            // membuat array untuk di transfer ke API
+            $result["success"] = "1";
+            $result["message"] = "success";
+            $this->response($result, 200);
+        } else {
+            // membuat array untuk di transfer ke API
+            $result["success"] = "0";
+            $result["message"] = "error";
+            $this->response(array($result, 502));
+        }
+    }
+
+    function ambil_data_murid_post()
+    {
+        $id_murid = $this->post('id_murid');
+
+        // variable array
+        $result = array();
+        $result['murid'] = array();
+
+        $data_id = array(
+            'id_murid' => $id_murid
+        );
+
+        // mengambil data dari database
+        $query = $this->M_universal->get_data('list_murid', $data_id);
+        if ($query->num_rows() > 0) {
+
+            // mengeluarkan data dari database
+            foreach ($query->result_array() as $row) {
+
+                // ambil detail data db
+                $data = array(
+                    'nama' => $row["nama"],
+                    'id_wali_murid' => $row["id_wali_murid"],
+                    'nama_wali_murid' => $row["nama_wali_murid"],
+                    'alamat' => $row["alamat"],
+                    'foto' => $row["foto"]
+                );
+
+                array_push($result['murid'], $data);
+
+                // membuat array untuk di transfer
+                $result["success"] = "1";
+                $result["message"] = "success berhasil mengambil data";
+                $this->response($result, 200);
+            }
+        } else {
+            // membuat array untuk di transfer ke API
+            $result["success"] = "0";
+            $result["message"] = "error data tidak ada";
+            $this->response($result, 502);
+        }
+    }
+
+    function update_murid_post()
+    {
+        $id_murid = $this->post('id_murid');
+        $id_wali_murid = $this->post('id_wali_murid');
+        $nama = $this->post('nama');
+        $foto = $this->post('foto');
+
+        $nama_foto = 'F' . $id_murid;
+
+        $data = array();
+
+        $data = array(
+            'id_wali_murid' => $id_wali_murid,
+            'nama'          => $nama,
+            'foto'          => $nama_foto
+        );
+
+        $where = array(
+            'id_murid' => $id_murid
+        );
+
+        if (!empty($foto)) {
+
+            $cek_foto = "";
+
+            // mengambil data dari database
+            $query = $this->M_universal->get_data('murid', $where);
+            foreach ($query->result_array() as $row) {
+
+                $cek_foto = $row["foto"];
+            }
+
+            if ($cek_foto != "DEFFMR") {
+                // lokasi gambar berada
+                $path = './upload/image/murid/';
+                $format = '.jpg';
+                unlink($path . $nama_foto . $format); // hapus data di folder dimana data tersimpan
+            }
+
+            $path2 = "./upload/image/murid/$nama_foto.jpg";
+            file_put_contents($path2, base64_decode($foto));
+        }
+
+        $update =  $this->M_universal->update_data($where, 'murid', $data);
+        if ($update) {
+
+            // membuat array untuk di transfer ke API
+            $result["success"] = "1";
+            $result["message"] = "success";
+            $this->response($result, 200);
+        } else {
+
+            // membuat array untuk di transfer ke API
+            $result["success"] = "0";
+            $result["message"] = "error";
+            $this->response(array($result, 502));
+        }
+    }
+
+    function delete_murid_post()
+    {
+        $id_murid = $this->post('id');
+
+        $where = array(
+            'id_murid' => $id_murid
+        );
+
+        $cek_foto = "";
+
+        // mengambil data dari database
+        $query = $this->M_universal->get_data('murid', $where);
+        foreach ($query->result_array() as $row) {
+            $cek_foto = $row["foto"];
+        }
+
+        if ($cek_foto != "DEFFMR") {
+
+            $nama_foto = 'F' . $id_murid;
+
+            // lokasi gambar berada
+            $path = './upload/image/murid/';
+            $format = '.jpg';
+            unlink($path . $nama_foto . $format); // hapus data di folder dimana data tersimpan
+        }
+
+        $hapus =  $this->M_universal->hapus_data($where, "murid");
+        if ($hapus) {
+
+            // membuat array untuk di transfer ke API
+            $result["success"] = "1";
+            $result["message"] = "success";
+            $this->response($result, 200);
+        } else {
+
+            // membuat array untuk di transfer ke API
+            $result["success"] = "0";
+            $result["message"] = "error";
+            $this->response(array($result, 502));
+        }
+    }
+}
