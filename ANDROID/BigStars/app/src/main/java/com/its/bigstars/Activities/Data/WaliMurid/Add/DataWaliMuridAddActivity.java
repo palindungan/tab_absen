a94e47b013b520fc387b55cc65540a79a69b0456
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-05-06 02:04:58 +0700

    persiapan activity pada data wali murd
    persiapan activity pada data wali murd

---
+++ ANDROID/BigStars/app/src/main/java/com/its/bigstars/Activities/Data/WaliMurid/Add/DataWaliMuridAddActivity.java
@@ -0,0 +1,16 @@
+package com.its.bigstars.Activities.Data.WaliMurid.Add;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.its.bigstars.R;
+
+public class DataWaliMuridAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_wali_murid_add);
+    }
+}
