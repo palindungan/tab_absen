e558843161454cb028a426a96672f8cb41cb5557
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-05-08 12:57:06 +0700

    persiapan data kelas
    persiapan data kelas

---
+++ ANDROID/BigStars/app/src/main/java/com/its/bigstars/Activities/Data/Kelas/Add/DataKelasAddActivity.java
@@ -0,0 +1,22 @@
+package com.its.bigstars.Activities.Data.Kelas.Add;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.its.bigstars.R;
+
+public class DataKelasAddActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_kelas_add);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ ANDROID/BigStars/app/src/main/java/com/its/bigstars/Activities/Data/Kelas/List/DataKelasListActivity.java
@@ -0,0 +1,22 @@
+package com.its.bigstars.Activities.Data.Kelas.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.its.bigstars.R;
+
+public class DataKelasListActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_kelas_list);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
